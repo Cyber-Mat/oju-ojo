be046caf3db1ff8fbfdf1f695a00c24ca585541a
Olamide Olaniyan <olaniyanolamide@gmail.com>
2021-04-23 10:04:49 +0100

    styling app background

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -1,24 +1,33 @@
 import { useState, useEffect } from 'react';
-import SearchPage from '../search-page/SearchPage';
 import './search.scss';
 
 const Search = () => {
   const [location, setLocation] = useState('london');
+  // const URL =
+  //   'http://api.ipstack.com/102.89.2.131?access_key=dd40220ecf3f68e68b82ccf505116689';
 
-  useEffect(() => {}, [setLocation]);
+  // const URL = 'https://cors-proxy.htmldriven.com/?url=https://ipapi.co/json/';
+
+  // const URL =
+  //   'https://api.openweathermap.org/data/2.5/weather?q=london&appid=1e2993fa366a3ffb691cc7a529865017';
+
+  useEffect(() => {
+    // fetch(URL)
+    //   .then(data => console.log(data))
+    //   .catch(error => console.log(error));
+  }, [setLocation]);
 
   return (
     <form className='search'>
-      <label htmlFor='search-input'>
-        <input
-          type='text'
-          id='search-input'
-          class='search-input'
-          value='london'
-          placeholder='Another location'
-          onChange={e => setLocation(e.target.value)}
-        />
-      </label>
+      <label htmlFor='search-input'>Another location</label>
+      <input
+        type='text'
+        id='search-input'
+        className='search-input'
+        value={location}
+        // placeholder='Another location'
+        onChange={e => setLocation(e.target.value)}
+      />
     </form>
   );
 };
